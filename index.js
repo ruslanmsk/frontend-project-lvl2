c2c81598893b77815fe4deb6a206941f095676bf
ruslankucherov <kr.vip.93@mail.ru>
2021-01-08 15:02:16 +0300

    change output logic

--- index.js
+++ index.js
@@ -4,24 +4,28 @@ export default function genDiff(filepath1, filepath2) {
   const file1 = parseFile(filepath1);
   const file2 = parseFile(filepath2);
 
+  const allKeys = [...new Set([...Object.keys(file1), ...Object.keys(file2)])];
+  allKeys.sort();
+
   const result = ['{'];
 
-  for (const [key, value] of Object.entries(file1)) {
-    if (key in file2) {
-      if (value === file2[key]) {
-        result.push(`    ${key}: ${value}`);
+  for (const key of allKeys) {
+    const isFile1Contain = key in file1;
+    const isFile2Contain = key in file2;
+
+    if (isFile1Contain) {
+      if (isFile2Contain) {
+        if (file1[key] === file2[key]) {
+          result.push(`    ${key}: ${file1[key]}`);
+        } else {
+          result.push(`  - ${key}: ${file1[key]}`);
+          result.push(`  + ${key}: ${file2[key]}`);
+        }
       } else {
-        result.push(`  - ${key}: ${value}`);
-        result.push(`  + ${key}: ${file2[key]}`);
+        result.push(`  - ${key}: ${file1[key]}`);
       }
     } else {
-      result.push(`  - ${key}: ${value}`);
-    }
-  }
-
-  for (const [key, value] of Object.entries(file2)) {
-    if (!(key in file1)) {
-      result.push(`  + ${key}: ${value}`);
+      result.push(`  + ${key}: ${file2[key]}`);
     }
   }
 
