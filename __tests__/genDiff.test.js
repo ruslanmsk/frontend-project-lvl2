84f41a0731f98c44dd8d514ac90393553988c2da
ruslankucherov <kr.vip.93@mail.ru>
2021-01-31 11:23:32 +0300

    delete babel and use esm

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,6 +1,10 @@
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
 import genDiff from '../index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const formats = ['json', 'yml'];
 
