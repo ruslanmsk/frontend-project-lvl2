ac15b1b171e5197ba6956fcaf05cb1723a6d74b2
ruslankucherov <kr.vip.93@mail.ru>
2021-01-31 11:54:07 +0300

    improve diff generator

--- src/diffGenerator.js
+++ src/diffGenerator.js
@@ -6,24 +6,25 @@ export default function generateDiff(json1, json2) {
   const allUniqueKeys = [...new Set([...keys1, ...keys2])];
 
   const result = _.sortBy(allUniqueKeys).map((key) => {
-    const isFile1Contain = key in json1;
-    const isFile2Contain = key in json2;
+    if (!_.has(json1, key)) {
+      return { status: 'added', key, value: json2[key] };
+    }
 
-    if (isFile1Contain) {
-      if (isFile2Contain) {
-        if (_.isObject(json1[key]) && _.isObject(json2[key])) {
-          return { status: 'complex', key, children: generateDiff(json1[key], json2[key]) };
-        }
-        if (json1[key] === json2[key]) {
-          return { status: 'unchanged', key, value: json1[key] };
-        }
-        return {
-          status: 'modified', key, oldValue: json1[key], newValue: json2[key],
-        };
-      }
+    if (!_.has(json2, key)) {
       return { status: 'deleted', key, value: json1[key] };
     }
-    return { status: 'added', key, value: json2[key] };
+
+    if (_.isObject(json1[key]) && _.isObject(json2[key])) {
+      return { status: 'complex', key, children: generateDiff(json1[key], json2[key]) };
+    }
+
+    if (json1[key] === json2[key]) {
+      return { status: 'unchanged', key, value: json1[key] };
+    }
+
+    return {
+      status: 'modified', key, oldValue: json1[key], newValue: json2[key],
+    };
   });
 
   return result;
